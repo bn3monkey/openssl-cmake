19b686bb240507dee9e34ab6896b4f3d8b084764
bn3monkey <bn3monkey@gmail.com>
2026-02-20 00:47:39 +0900

    - add android compile

--- prj/android/app/build.gradle.kts
+++ prj/android/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.bn3monkey.openssl_cmake"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.bn3monkey.openssl_cmake"
         minSdk = 35
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
